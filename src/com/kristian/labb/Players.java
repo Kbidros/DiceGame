32fc919c424fb7bb19d404b353d1a3ea0a825e5f
Kbidros <kristianbidros@gmail.com>
2023-10-02 21:23:34 +0200

    First commit

---
+++ src/com/kristian/labb/Players.java
@@ -0,0 +1,16 @@
+package com.kristian.labb;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Players {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Choose number of players!");
+
+    }
+
+}
